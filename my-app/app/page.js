5bf9be24919fa720d4329a93f6a77b81e9af53e6
Mathakgale Mohale <mathakgale.mohale@sosimpleenergy.co.za>
2026-06-09 19:52:57 +0200

    added web app folder structure

---
+++ my-app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to My App</h1>
+      <p>Get started by editing app/page.js</p>
+    </main>
+  )
+}
---
+++ my-app/components/layout/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/dashboard">Dashboard</a>
+        <a href="/auth">Login</a>
+      </nav>
+    </header>
+  )
+}
